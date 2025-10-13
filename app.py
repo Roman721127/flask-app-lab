ba60f22dfe20861cfd9df311dbc54d24fa9788df
Roman_Orihivskij <@rorihivskij>
2025-10-13 14:26:54 +0300

    Lab2: Flask website with Resume and Contacts pages

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+from config import SECRET_KEY, FLASK_DEBUG
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
+
 
 @app.route('/')
-def main():
-    return 'Hello, world!'
+@app.route('/resume')
+def resume():
+    return render_template('resume.html', title="Моє резюме")
+
+
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html', title="Контакти")
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=FLASK_DEBUG)
