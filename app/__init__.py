e2f99aa41cacdc6c6289bb0646098b38ea23aed0
Roman_Orihivskij <@rorihivskij>
2025-11-21 21:00:44 +0200

    Lab 7: Complete database migrations, products table and history

--- app/__init__.py
+++ app/__init__.py
@@ -3,20 +3,28 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect
+from sqlalchemy import MetaData 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-db = SQLAlchemy(app)
+convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+metadata = MetaData(naming_convention=convention)
+
+db = SQLAlchemy(app, metadata=metadata)
 migrate = Migrate(app, db)
 csrf = CSRFProtect(app)
 
 from app import models
-
 from app import views
 from app.users import bp as users_bp
 from app.products import bp as products_bp
-
 from app.posts import bp as posts_bp
 
 app.register_blueprint(users_bp, url_prefix='/users')
